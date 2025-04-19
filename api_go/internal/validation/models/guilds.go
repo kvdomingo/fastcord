47f8826ba251f8aae49a4142c4b608eab71b8cf4
Kenneth V. Domingo <hello@kvd.studio>
2025-04-19 23:22:45 +0800

    refactor: remove legacy code

--- api_go/internal/validation/models/guilds.go
+++
@@ -1,7 +0,0 @@
-package models
-
-type CreateGuildInput struct {
-	Name   string  `json:"name" validate:"required"`
-	Avatar *string `json:"avatar" validate:"omitempty,url"`
-	Banner *string `json:"banner" validate:"omitempty,url"`
-}
--- api_go/internal/validation/models/pagination.go
+++
@@ -1,6 +0,0 @@
-package models
-
-type PaginationParams struct {
-	Page     int `json:"page" validate:"gte=1"`
-	PageSize int `json:"page_size" validate:"gte=1,lte=50"`
-}
